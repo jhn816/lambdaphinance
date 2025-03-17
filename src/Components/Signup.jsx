6c385980f236c74678c64f638b9e26f49b83d0c4
jhn816 <jhn3@buffalo.edu>
2025-03-17 08:35:18 -0400

    Setting up home and signup

---
+++ src/Components/Signup.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./css/Signup.css"
+
+const Signup = () => {
+    return (
+        <div className="signup-containers">
+            
+        </div>
+    )
+}
+
+export default Signup;
