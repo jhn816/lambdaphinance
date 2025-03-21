47aff58bab041c11d92e0052727fcc03a763c2c9
jhn816 <jhn3@buffalo.edu>
2025-03-20 20:52:29 -0400

    index typo

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(cors({
 
 app.use(express.json());
 
-const mongo_url = proncess.env.MONGO_URL;
+const mongo_url = prncess.env.MONGO_URL;
 const accounts = mongoose.createConnection(mongo_url, {
     dbName:"accounts",
     useNewUrlParser: true,
