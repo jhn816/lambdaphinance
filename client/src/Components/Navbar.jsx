502836cd7b62e5fc6e58a534ba53d94fd0952370
jhn816 <jhn3@buffalo.edu>
2025-07-03 19:53:27 -0400

    Hamburger menu added for mobile, still need to style it up

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -2,12 +2,25 @@ import React, { useEffect, useState } from "react";
 import "./css/Navbar.css";
 import userEvent from "@testing-library/user-event";
 import { Link } from "react-router-dom";
+import Hamburger from "../assets/hamburg.png";
+import Close from "../assets/closeham.png";
 
 
 const Navbar = () => {
+    const [ham, setHam] = useState(false);
+    const [closing, setClosing] = useState(false);
 
-    return (
-        <div className="navigation-bar">
+
+    const handleClose = () => {
+        setClosing(true);
+        setTimeout(() => {
+            setHam(!ham);
+            setClosing(false);
+        }, 500);
+      };
+
+    return ( <>
+        <div className="desktop-bar">
             <ul className = "nav-links">
                 <a href="/">Home</a>
 
@@ -22,6 +35,26 @@ const Navbar = () => {
                 <a className="profile-nav" href="profile">Profile</a>
             </ul>
         </div>
+
+        <div className="mobile-bar">
+            <ul className = "nav-links" style={{flexDirection:"column", justifyContent:"center"}}>
+                {!ham && <img src={Hamburger} alt="hamburger-menu" onClick={() => setHam(!ham)}/>}
+                {ham && <img src={Close} alt="hamburger-menu" onClick={handleClose}/>}
+            </ul>
+        </div>
+
+        { ham && <div className={closing ? "hamburger-close" : "hamburger-open"}>
+        <a href="/">Home</a>
+        {/* expenses tracker of your own account, with total amounts and separate net gain/loss*/}
+        <a href="expenses">Expenses</a>
+        {/* debts owed to or form, accounts can request from each other, also where friends list is and you can share your expenses*/}
+        <a href="records">Records</a>
+        {/* financials goals you wanna set, with graphs and charts*/}
+        <a href="plan">Plan</a>
+        <a className="about-nav" href="about">About</a>
+        <a className="profile-nav" href="profile">Profile</a>
+        </div> }
+        </>
     )
 
 }
